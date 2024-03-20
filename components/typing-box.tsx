16f2838ff54576b0d877f589ddb087dc31e1fe24
Lakshk25 <raghavkumrawat01039@gmail.com>
2024-03-20 12:14:12 +0530

    only correct words green

--- components/typing-box.tsx
+++ components/typing-box.tsx
@@ -1,26 +1,33 @@
 "use client";
+import { cn } from "@/lib/utils";
 import React, { useState } from "react";
 
 const TypingTest = () => {
   const initialWords = ["work", "is", "the", "best", "medicine", "that", "can,", "reduce", "anxiety", "of", "workload", "sometimes"];
   const [words, setWords] = useState(initialWords);
   const [inputValue, setInputValue] = useState("");
-  const [completedWords, setCompletedWords] = useState([]);
+  const [completedWords, setCompletedWords] = useState<string[]>([]);
+  const [isWordCorrect, setIsWordCorrect] = useState(true);
 
-  const handleInputChange = (event) => {
+  // for input 
+  const handleInputChange = (event: any) => {
     const { value } = event.target;
     setInputValue(value);
   };
 
-  const handleKeyDown = (event) => {
+  const handleKeyDown = (event: any) => {
     if (event.key === " ") {
-      event.preventDefault();
-      const trimmedInputValue = inputValue.trim();
+      event.preventDefault(); // prevent space default behaviour (adding space)
+      const trimmedInputValue = inputValue.trim();  // Remove leading and trailing whitespace from the input value
       if (trimmedInputValue === words[0]) {
         setCompletedWords((prevCompletedWords) => [...prevCompletedWords, words[0]]);
         setInputValue(""); // Clear the input field
-        setWords((prevWords) => prevWords.slice(1)); // Remove the completed word
       }
+      else {
+        setInputValue("");
+        setIsWordCorrect(false);
+      }
+      setWords((prevWords) => prevWords.slice(1)); // Remove the completed word
     }
   };
 
@@ -30,9 +37,7 @@ const TypingTest = () => {
         {initialWords.map((word, index) => (
           <span
             key={index}
-            style={{
-              color: completedWords.includes(word) ? "green" : "black",
-            }}
+            className={cn(completedWords.includes(word) ? "text-green-500" : "text-black")}
           >
             {word}{" "}
           </span>
